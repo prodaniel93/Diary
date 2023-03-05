7b72f1fb1c46e992806192bef8a68d3242024045
HwangJongSoo <ghkdwhdtn9@naver.com>
2023-03-05 16:56:31 +0900

    author
    다운될까봐 미리 commit

---
+++ simplediary/src/DiaryEditor.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const DiaryEditor = () => {
+  const [author, setAuthor] = useState('황종수');
+
+  return (
+    <div className='DiaryEditor'>
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          value={author}
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
