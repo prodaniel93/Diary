657327214cb517778f86588857cbe92d8d6dfabe
HwangJongSoo <ghkdwhdtn9@naver.com>
2023-03-07 15:25:16 +0900

    useRef + List 세팅
    useRef를 이용해서 DOM 요소를 선택해보자

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,31 +1,66 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+// useRef : DOM 요소를 선택할수 있는 기능
 
 const DiaryEditor = () => {
-  const [author, setAuthor] = useState('황종수');
-  const [content, setContent] = useState('');
+  // 동작이 비슷한 state는 하나로 묶을수 있음.
+  const authorInput = useRef();
+  const contentInput = useRef();
+
+  const [state, setState] = useState({
+    author: '',
+    content: '',
+    emotion: 1,
+  });
+
+  // const [author, setAuthor] = useState('');
+  // const [content, setContent] = useState('');
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      alert('작성자는 최소 1글자 이상 입력해주세요');
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      alert('일기 본문은 최소 1글자 이상 입력해주세요');
+      // focus하기
+      contentInput.current.focus();
+      return;
+    }
+    alert('저장 성공!');
+  };
 
   return (
     <div className='DiaryEditor'>
       <h2>오늘의 일기</h2>
+
       <div>
-        <input
-          name='author'
-          value={author}
-          onChange={(e) => {
-            // console.log(e.target.value);
-            // console.log(e.target.name);
-            setAuthor(e.target.value);
-          }}
-        />
+        <input ref={authorInput} name='author' value={state.author} onChange={handleChangeState} />
       </div>
+
       <div>
-        <textarea
-          value={content}
-          onChange={(e) => {
-            // console.log(e.target.value);
-            setContent(e.target.value);
-          }}
-        />
+        <textarea ref={contentInput} name='content' value={state.content} onChange={handleChangeState} />
+      </div>
+
+      <div>
+        <select name='emotion' value={state.emotion} onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
